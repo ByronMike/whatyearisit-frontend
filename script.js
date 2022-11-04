13a27787ca4b9b5dad1687ed3053bae52622f5e1
Michael <auger.michaell@gmail.com>
2022-11-04 19:57:48 +0100

    commit

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// function callDate() {
-//       document.querySelector("#body").innerHTML += `
-//         <p id="year"></p>
-//         `;
-// }
-
-// callDate();
-
 fetch("https://whatyearisit-backend-three.vercel.app/date")
   .then((response) => response.json())
   .then((data) => {
